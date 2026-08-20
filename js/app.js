38b1378edf992f97d4d26833dfd8354752763abf
Kozhurin Dmytro <72601446+DDmitroIDD@users.noreply.github.com>
2026-08-20 16:12:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import { initMap } from './modules/map.js';
 import { initGeolocation } from './modules/geolocation.js';
-import { loadWaterways } from './modules/waterways.js';
+import { loadWaterways, setNonNavigableVisible } from './modules/waterways.js';
 import { loadAccessPoints } from './modules/accessPoints.js';
 import { initI18n } from './modules/i18n.js';
 
@@ -14,6 +14,14 @@ loadAccessPoints(map);
 document.getElementById('locate-btn').addEventListener('click', () => {
   initGeolocation(map);
 });
+
+const toggleEl = document.getElementById('toggle-non-navigable');
+if (toggleEl) {
+  setNonNavigableVisible(map, toggleEl.checked);
+  toggleEl.addEventListener('change', (e) => {
+    setNonNavigableVisible(map, e.target.checked);
+  });
+}
 
 window.addEventListener('i18n:changed', () => {
   loadAccessPoints(map);
